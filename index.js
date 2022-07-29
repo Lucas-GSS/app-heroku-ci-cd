9565445706f00ef2bc38d7f077301851197f0ada
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-07-28 22:11:02 -0300

    referência a GoT

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.send(`<h1>Rodando na porta ${PORT}</h1>`));
+app.get('/', (req, res) => res.send('<h1>The girl is Arya Stark of Winterfell. And i\'m going home.</h1>'));
 
 app.listen(PORT);
